cf836b8ed855909216cb7e0ca84a7773c7c7aa7e
Avinash <javinash140@gmail.com>
2023-07-29 16:57:40 +0530

    .

---
+++ Basic JS Programs/remove.js
@@ -0,0 +1,11 @@
+//Remove a specific element from an array:
+function removeElement(arr, element) {
+  return arr.filter(item => item !== element);
+}
+
+console.log(removeElement([1,2,3,4,1,3,1],1))
+
+
+git add .
+git commit -m "."
+git push 
