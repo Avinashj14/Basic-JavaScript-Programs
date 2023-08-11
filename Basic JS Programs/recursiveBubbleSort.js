9e8fd7e70d28da6ed8ecede1a35fcb3cdfdb8a90
Avinash <javinash140@gmail.com>
2023-08-11 23:44:00 +0530

    Recursive bubble Sort \

--- Basic JS Programs/recursiveBubbleSort.js
+++ Basic JS Programs/recursiveBubbleSort.js
@@ -1,17 +1,17 @@
 
 
+let countLoopRun = 0;
 function recursiveBubbleSort(data, count) {
   if (count == 1) {
-    return;
+    return countLoopRun;
   }
-  let currentEl = 0;
   
   for (let i = 0; i < count - 1; i++) {
     if (data[i] > data[i + 1]) {
       let temp = data[i];
       data[i] = data[i + 1];
       data[i + 1] = temp;
-      currentEl++;
+      countLoopRun++;
     }
   }
   recursiveBubbleSort(data,count-1)
@@ -19,3 +19,4 @@ function recursiveBubbleSort(data, count) {
 let arr = [64, 34, 25, 12];
 recursiveBubbleSort(arr, arr.length)
 console.warn(arr)
+console.warn(countLoopRun)
